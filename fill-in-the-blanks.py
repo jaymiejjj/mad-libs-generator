4c3c8aa93275956d0804a8ce52a300ebae168e05
jaymie <jaymie9019@gmail.com>
2017-05-16 21:57:21 +0800

    modified fill-in-the-blanks.py

--- fill-in-the-blanks.py
+++ fill-in-the-blanks.py
@@ -5,45 +5,49 @@ sys.setdefaultencoding('utf-8')
 
 
 def generate_quiz():
-    # 创建测验问题的字典
-    tips0 = u"这个八岁的男孩酷爱弹钢琴，以致于他坚持练琴三年了。"
-    sample0 = "The eight-year-old girl likes __1__ the piano __2__ much __3__ he has kept __4__ for three years."
-    answer0 = ['playing', 'so', 'that', 'practicing']
+    '''Create a quiz dict.
 
-    tips1 = u"由于天气不好，校运会不得不推迟。"
-    sample1 = "__1__ __2__ the bad weather, the school sports meet had to __3__ __4__ __5__."
-    answer1 = ['Because', 'of', 'be', 'put', 'off']
+    Returns:
+        a quiz dict,the key is index and the value is the list with tip, sample and answer
+    '''
 
-    tips2 = u"刘老师是位非常亲切的老师，以致于我们把她当做自己的母亲。"
-    sample2 = "Mrs. Liu is __1__ __2__ kind teacher__3__ we __4__ her __5__ our mother."
-    answer2 = ['such', 'a', 'that', 'regard', 'as']
+    tips = [u"这个八岁的女孩酷爱弹钢琴，以致于他坚持练琴三年了。",
+            u"由于天气不好，校运会不得不推迟。",
+            u"刘老师是位非常亲切的老师，以致于我们把她当做自己的母亲。",
+            u"下定决心努力学习吧,你迟早会成功的。"]
 
-    tips3 = u"下定决心努力学习吧,你迟早会成功的。"
-    sample3 = "Make __1__ __2__ __3__to work hard, __4__ you‘ll succeed __5__ __6__ __7__."
-    answer3 = ['up', 'your', 'mind', 'and', 'soon', 'or', 'later']
+    samples = ["The eight-year-old girl likes __1__ the piano __2__ much __3__ he "
+               "has kept __4__ for three years.",
+               "__1__ __2__ the bad weather, the school sports meet had to __3__ __4__ __5__.",
+               "Mrs. Liu is __1__ __2__ kind teacher__3__ we __4__ her __5__ our mother.",
+               "Make __1__ __2__ __3__to work hard, __4__ you‘ll succeed __5__ __6__ __7__."]
 
-    tips = [tips0, tips1, tips2, tips3]
-    samples = [sample0, sample1, sample2, sample3]
-    answers = [answer0, answer1, answer2, answer3]
+    answers = [['playing', 'so', 'that', 'practicing'],
+               ['Because', 'of', 'be', 'put', 'off'],
+               ['such', 'a', 'that', 'regard', 'as'],
+               ['up', 'your', 'mind', 'and', 'soon', 'or', 'later']]
 
-    quiz_zip = {}
-    index = 0
-    for tip, sample, answer in zip(tips, samples, answers):
-        quiz_zip[index] = [tip, sample, answer]
-        index += 1
+    quiz_zip = {index: [tip, sample, answer] for index, tip, sample, answer in
+                zip(range(0, len(tips)), tips, samples, answers)}
 
     return quiz_zip
 
 
 def get_level(quiz_num):
-    # 获取用户输入的等级
+    '''
+    Get user's choice
+    :param quiz_num: user's input which need a integer
+    :return: a level num
+    '''
+
     print u'''###################################
 欢迎来到单词填空挑战,如果中途您想退出游戏，请输入end
 0 - 简单
 1 - 中等
 2 - 苦难
 3 - 专家级'''
-    level = raw_input(u"请选择您要挑战的难度（请输入数字0-{quiz_num}）： ".format(quiz_num=quiz_num - 1))
+    level = raw_input(u"请选择您要挑战的难度（请输入数字0-{quiz_num}）： ".
+                      format(quiz_num=quiz_num - 1))
 
     while True:
         if not level.isdigit():
@@ -59,12 +63,22 @@ def get_level(quiz_num):
 
 
 def get_quiz(level, quiz_zip):
-    # 根据用户输入的等级返回相应的quiz
+    '''
+    Get the quiz which user choose
+    :param level: an integer,which user input
+    :param quiz_zip: all quiz 
+    :return: the quiz which usr choose including tip, sample and answer
+    '''
+
     return quiz_zip[level]
 
 
 def get_alllow_wrong_time():
-    # 获取允许用户输错的最大次数
+    '''
+    Get the the allowed wrong times 
+    :return: an integer 
+    '''
+
     wrong_times = raw_input(u"请输入您可以允许自己回答错误的次数（1-10）")
 
     while True:
@@ -81,16 +95,29 @@ def get_alllow_wrong_time():
 
 
 def get_blank_num(sample):
-    # 获取改问题中需要回答的问题个数
+    '''
+    Get the blanks number in the sample
+    :param sample: a sample in a quiz
+    :return: balnk number,an integer,
+    '''
+
     sample_lst = sample.split()
     return len([i for i in sample_lst if "__" in i])
 
 
 def fill_blank(tip, sample, answer, wrong_times):
-    # 开始填写问题
+    '''
+    the function deal user's input 
+    :param tip: the quiz tip
+    :param sample: the quiz sample
+    :param answer: the quiz answer
+    :param wrong_times: allow max wrong times
+    :return: if user answer the all blanks in allowed wrong times, return success or return failed
+    '''
+
     print "###################################"
-    print u"{tip}".format(tip=tip)
-    print "{sample}".format(sample=sample)
+    print tip
+    print sample
 
     count = 0
     while True:
@@ -106,7 +133,8 @@ def fill_blank(tip, sample, answer, wrong_times):
 
             if count == wrong_times:
                 break
-            print(u"恭喜您第{n}个单词填写正确,您还有{times}次机会".format(times=wrong_times-count, n=n+1))
+            print(u"恭喜您第{n}个单词填写正确,您还有{times}次机会".
+                  format(times=wrong_times-count, n=n+1))
             sample = sample.replace("__{n}__".format(n=n+1), input_answer)
             print sample
 
@@ -119,8 +147,11 @@ def fill_blank(tip, sample, answer, wrong_times):
         print u"很遗憾，挑战失败，您已经没有机会再挑战了！"
 
 
-
 def main():
+    '''
+    The main function which combined all functions logic
+    '''
+
     quiz_zip = generate_quiz()
     quiz_num = len(quiz_zip)
     level = get_level(quiz_num)
